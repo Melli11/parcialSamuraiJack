57064428960376fe0196a0b4a4d925339f454f59
Melli11 <mingerez@gmail.com>
2022-05-19 15:34:34 -0300

    Avances Parcial

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,117 @@
 module Library where
 import PdePreludat
+import GHC.Num (Num, subtract)
+import Data.Type.Coercion (TestCoercion)
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+
+data Elemento = UnElemento {
+    tipo :: String,
+    ataque :: Personaje-> Personaje,
+    defensa :: Personaje -> Personaje
+    } deriving (Show )
+
+data Personaje = UnPersonaje {
+    nombre :: String,
+    salud :: Number,
+    elementos :: [Elemento],
+    anioPresente :: Number
+    } deriving (Show )
+
+-- Empecemos por algunas transformaciones básicas:
+-- a. mandarAlAnio: lleva al personaje al año indicado.
+
+mandarAlAnio :: Number -> Personaje -> Personaje
+mandarAlAnio  año unPersonaje = unPersonaje { anioPresente = año}
+
+-- b. meditar: le agrega la mitad del valor que tiene a la salud del personaje.
+
+meditar :: Personaje -> Personaje
+meditar unPersonaje  = modificarSaludDelPersonaje (salud unPersonaje *0.5) unPersonaje
+
+modificarSaludDelPersonaje :: Number -> Personaje -> Personaje
+modificarSaludDelPersonaje cantidad unPersonaje = unPersonaje { salud = salud unPersonaje + cantidad}
+
+-- c. causarDanio: le baja a un personaje una cantidad de salud dada.
+-- Hay que tener en cuenta al modificar la salud de un personaje que ésta nunca puede
+-- quedar menor a 0.
+
+causarDanio :: Number -> Personaje -> Personaje
+causarDanio cantidad unPersonaje
+    |   laSaludSeráMayorA0 cantidad unPersonaje =  modificarSaludDelPersonaje (negate cantidad) unPersonaje
+    |   otherwise = unPersonaje
+-- Importante:no repetir lógica.
+
+laSaludSeráMayorA0 :: Number -> Personaje -> Bool
+laSaludSeráMayorA0 cantidad unPersonaje = salud unPersonaje - cantidad > 0
+
+--  Queremos poder obtener algo de información extra sobre los personajes. Definir las
+-- siguientes funciones:
+
+-- data Elemento = UnElemento {
+--     tipo :: String,
+--     ataque :: Personaje-> Personaje,
+--     defensa :: Personaje -> Personaje
+--     }
+
+
+-- data Personaje = UnPersonaje {
+--     nombre :: String,
+--     salud :: Number,
+--     elementos :: [Elemento],
+--     anioPresente :: Number
+--     }
+-- a. esMalvado, que retorna verdadero si alguno de los elementos que tiene el personaje
+-- en cuestión es de tipo “Maldad”.
+
+esMalvado :: Personaje -> Bool
+esMalvado  = elem "Maldad" . listaDeTiposDeElementos
+
+listaDeTiposDeElementos :: Personaje -> [String]
+listaDeTiposDeElementos unPersonaje = map tipo $elementos unPersonaje
+
+-- b. danioQueProduce :: Personaje -> Elemento -> Float, que retorne la diferencia entre
+-- la salud inicial del personaje y la salud del personaje luego de usar el ataque del
+-- elemento sobre él.
+
+danioQueProduce :: Personaje -> Elemento -> Number
+danioQueProduce unPersonaje elemento = diferenciaEntre  (salud unPersonaje)  $ salud (ataque elemento unPersonaje)
+
+diferenciaEntre :: Number -> Number -> Number
+diferenciaEntre saludInicial saludFinal =  saludInicial - saludFinal
+
+realizarAtaqueSobrePersonaje :: Elemento -> Personaje -> Personaje
+realizarAtaqueSobrePersonaje = ataque
+
+-- -- Test 
+-- *Spec Library Spec> realizarAtaqueSobrePJ bolaDeFuego darthVader 
+-- UnPersonaje {nombre = "Darth Vader", salud = 90, elementos = [], anioPresente = 2022}
+-- *Spec Library Spec> darthVader 
+-- UnPersonaje {nombre = "Darth Vader", salud = 100, elementos = [], anioPresente = 2022
+
+
+-- c.enemigosMortalesque dado un personaje y una lista de enemigos, devuelve la lista
+-- de los enemigos que pueden llegar a matarlo con un solo elemento. Esto sucede si
+-- luego de aplicar el efecto de ataque del elemento, el personaje queda con salud igual a
+-- 0.
+
+enemigosMortalesque :: Personaje -> [Personaje] -> [Personaje]
+enemigosMortalesque personaje = filter (puedeLLegarAMatarloConAlgunElemento personaje )
+
+puedeLLegarAMatarloConAlgunElemento :: Personaje -> Personaje -> Bool
+puedeLLegarAMatarloConAlgunElemento unPersonaje enemigo = any ((==0). danioQueProduce unPersonaje) $ elementos enemigo
+
+ataqueDeUn :: Personaje -> [Personaje -> Personaje]
+ataqueDeUn personaje = map ataque $elementos personaje
+
+
+
+-- VARIABLES AUXILIARES
+
+bolaDeFuego :: Elemento
+bolaDeFuego = UnElemento "Fuego" (causarDanio 10) (mandarAlAnio 0)
+
+darthVader :: Personaje
+darthVader = UnPersonaje "Darth Vader" 100 [] 2022
--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,15 @@ import PdePreludat
 import Library
 import Test.Hspec
 
-correrTests :: IO ()
-correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+-- correrTests :: IO ()
+-- correrTests = hspec $ do
+    -- suiteDeTestsDeParte1
+    -- suiteDeTestsDeParte2
+    -- suiteDeTestsDeParte3
+    -- suiteDeTestsDeParte4
+
+-- suiteDeTestsDeParte1 = describe "Parte 1: Transformaciones basicas " $ do
+--   describe "mandarAlAnio" $ do
+--       it "Mandar al anio 10 envia a un personaje a ese anio " $ do
+--         mandarAlAnio (UnPersonaje "Darth Vader" 100 [] 2022) `shouldBe` (UnPersonaje "Darth Vader" 100 [] 10) --las funciones no se pueden ordenar ni comparar 
 
